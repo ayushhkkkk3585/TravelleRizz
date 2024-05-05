c6f1bd3eee9ee67113e34faea73c9c7965862316
ayushhkkkk3585 <136594718+ayushhkkkk3585@users.noreply.github.com>
2024-05-05 23:38:05 +0530

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -33,7 +33,7 @@ const Main = () => {
                     </form>
                 </div>
             </div>
-            <div className='font-bold text-xl my-5 text-center underline'>Contributors: Parshad, Divya, Ayush</div>
+            <div className='font-bold text-xl my-5 text-center underline'>Made with Love for YOU ✈️ </div>
 
 
 
